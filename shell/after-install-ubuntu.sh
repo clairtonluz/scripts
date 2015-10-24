d8484740232a795e6b68e663a4f696f8f45aec64
clairtonluz <clairton.c.l@gmail.com>
2015-10-23 21:24:54 -0300

    adicionando instalação do gimp

--- shell/after-install-ubuntu.sh
+++ shell/after-install-ubuntu.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
-apt-get update -y
-apt-get upgrade -y
-apt-get dist-upgrade -y
-apt-get install git -y
-apt-get install geany -y
-apt-get install htop -y
-apt-get install vim -y
-apt-get install rake -y
-apt-get install mongodb -y 
-apt-get install nautilus-open-terminal -y
-#INSTALL VIM JANUS
-curl -Lo- https://bit.ly/janus-bootstrap | bash
-# INSTALL NVM
-apt-get install build-essential libssl-dev
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.27.1/install.sh | bash
+{
+  echo "This script requires superuser access to install apt packages."
+  echo "You will be prompted for your password by sudo."
 
-# corrige bug do npm install fonte: http://stackoverflow.com/questions/21155922/error-installing-node-gyp-on-ubuntu
-apt-get install libicu-dev -y
+  # clear any previous sudo permission
+  sudo -k
 
-git config --global user.name clairtonluz
-git config --global user.email clairton.c.l@gmail.com
-git config --global credential.helper 'cache --timeout=28800'
+  # run inside sudo
+  sudo bash <<SCRIPT
+  apt-get update -y
+  apt-get upgrade -y
+  apt-get dist-upgrade -y
+  apt-get install git -y
+  apt-get install geany -y
+  apt-get install htop -y
+  apt-get install vim -y
+  apt-get install rake -y
+  apt-get install mongodb -y
+  apt-get install gimp -y
+  apt-get install nautilus-open-terminal -y
+  #INSTALL VIM JANUS
+  curl -Lo- https://bit.ly/janus-bootstrap | bash
+  # INSTALL NVM
+  apt-get install build-essential libssl-dev
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.27.1/install.sh | bash
+
+  # corrige bug do npm install fonte: http://stackoverflow.com/questions/21155922/error-installing-node-gyp-on-ubuntu
+  apt-get install libicu-dev -y
+
+  git config --global user.name clairtonluz
+  git config --global user.email clairton.c.l@gmail.com
+  git config --global credential.helper 'cache --timeout=28800'
+  SCRIPT
+}
