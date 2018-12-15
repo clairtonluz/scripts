29542b411284cd1eb1f15148f057260a93fee5ee
Clairton Luz <clairton.c.l@gmail.com>
2018-12-15 15:02:42 -0300

    Update after-install-fedora.sh

--- shell/after-install-fedora.sh
+++ shell/after-install-fedora.sh
@@ -3,13 +3,13 @@ sudo dnf update -y
 sudo dnf upgrade -y
 sudo dnf dist-upgrade -y
 
-# RPM Fusion no Fedora 28
-sudo dnf install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-28.noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-28.noarch.rpm
+# RPM Fusion no Fedora 29
+sudo dnf install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-29.noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-29.noarch.rpm
 
 # Instale os codecs multimídia do RPM Fusion
 sudo dnf install amrnb amrwb faad2 flac ffmpeg gpac-libs lame libfc14audiodecoder mencoder mplayer x264 x265 gstreamer-plugins-espeak gstreamer-plugins-fc gstreamer-rtsp gstreamer-plugins-good gstreamer-plugins-bad gstreamer-plugins-bad-free-extras gstreamer-plugins-bad-nonfree gstreamer-plugins-ugly gstreamer-ffmpeg gstreamer1-plugins-base gstreamer1-libav gstreamer1-plugins-bad-free-extras gstreamer1-plugins-bad-freeworld gstreamer1-plugins-base-tools gstreamer1-plugins-good-extras gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-good
 
-sudo dnf -y install vlc vim git geany htop gimp wine postgresql-server
+sudo dnf -y install vim git geany htop gimp wine
 
 # Google Chrome
 sudo dnf install https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
@@ -25,7 +25,7 @@ nvm install --lts
 git config --global user.name clairtonluz
 git config --global user.email clairton.c.l@gmail.com
 # salva a senha por 8 horas
-git config --global credential.helper 'cache --timeout=28800'
+git config --global credential.helper 'cache --timeout=29800'
 
 # Vim como editor padrão
  echo 'export EDITOR=/usr/bin/vim' >> ~/.bashrc
