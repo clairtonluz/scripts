b6691b8ea8b6888d81b7983289690b2d2b73ae0c
Clairton Carneiro Luz <clairton.luz@seduc.ce.gov.br>
2019-11-19 14:49:03 -0300

    asdf plugins

---
+++ shell/asdf.sh
@@ -0,0 +1,26 @@
+# Lista todos os plugins disponiveis
+asdf plugin-list-all
+
+asdf plugin-add flutter
+asdf list-all flutter
+asdf install flutter 1.9.1+hotfix.6-stable
+
+# plugins
+
+flutter          https://github.com/oae/asdf-flutter.git
+java             https://github.com/halcyon/asdf-java.git
+mongodb          https://github.com/sylph01/asdf-mongodb.git
+mysql            https://github.com/iroddis/asdf-mysql.git
+neovim           https://github.com/richin13/asdf-neovim.git
+nodejs          *https://github.com/asdf-vm/asdf-nodejs.git
+php              https://github.com/odarriba/asdf-php.git
+postgres         https://github.com/smashedtoatoms/asdf-postgres.git
+python           https://github.com/danhper/asdf-python.git
+
+kube-capacity    https://github.com/looztra/asdf-kube-capacity.git
+kubectl          https://github.com/Banno/asdf-kubectl.git
+kubectl-bindrole  https://github.com/looztra/asdf-kubectl-bindrole.git
+kubefedctl       https://github.com/kvokka/asdf-kubefedctl.git
+kubeseal         https://github.com/stefansedich/asdf-kubeseal.git
+kubesec          https://github.com/vitalis/asdf-kubesec.git
+kubeval          https://github.com/stefansedich/asdf-kubeval.git
